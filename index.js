d066fce819f21fd169550298e1a88214157624cf
Izu <tushar.jamwal@gmail.com>
2020-05-24 16:16:33 +0530

    First skeleton prototype

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const bot = Discord.Client()
+const token = ''
+
+const PREFIX= 'wca?'
+
+bot.on('message', msg=>{
+    if(msg.content.substring(0,PREFIX.length-1)===PREFIX){
+
+        let args = msg.content.substring(PREFIX.length).split(" ");
+        switch(args[0]){
+            case 'help':
+                //insert code for help command here
+                break;
+            
+            case 'test':
+                msg.reply("I am listening");
+                break;
+            case 'search':
+
+            //add code to search for people using https://www.worldcubeassociation.org/api/v0/search/users?persons_table=true&q=
+    }
+})
+
+bot.login(token)
