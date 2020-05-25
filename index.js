578a76e0e213bb7dc7428ca4b581c2c8fadfc123
Izu <tushar.jamwal@gmail.com>
2020-05-25 13:00:11 +0530

    added config file and cleaned up the code
    Will work on creating a person object type and a rich embed to display that in chat.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
-
+const { PREFIX, token } = require('./config.json');
 
 const bot = new Discord.Client();
-const token = '';
-
-const PREFIX= 'wca?';
 
 const apiUrl = 'https://www.worldcubeassociation.org/api/v0/';
 const search = 'search/users?persons_table=true&q=';
@@ -20,20 +17,23 @@ bot.on('message', async msg=>{
         switch(args[0]){
             //a case for every supported bot command
 
-            case 'help':
-                //insert code for help command here
-            break;
-            
-            case 'test':
-                msg.reply("I am listening");
-            break;
-
             case 'search':
                 qry = args[1];
                 let requestUrl = apiUrl+search+qry;
-                const result = await fetch(requestUrl).then(response => response.text());
-                displayName(JSON.parse(result), msg);
-            break;
+                const result = await fetch(requestUrl).then(response => response.json());
+                msg.reply(cuberName(result));
+                break;
+
+            case 'help':
+                //insert code for help command here
+                break;
+                
+            case 'test':
+                msg.reply("I am listening");
+                break;
+
+            case:
+                msg.reply('That is not a valid command. Type '+ PREFIX + 'help for a list of commands.')
         }
     }    
 })
@@ -41,11 +41,11 @@ bot.on('message', async msg=>{
 //declare functions here.
 /*The functionality of search can be moved here
 making the switch structure for commands cleaner and easier to understand*/
-
-function displayName(jsonObj, message){
+//I will put the list of functions that accepts json objects as arguments under this
+//have to detach the discord message from this
+function cuberName(jsonObj){
     let result = jsonObj['result'];
     let cuber = result[0];
-    let cubername = cuber['name'];
-    message.channel.send(cubername);
+    return cuber['name'];
 }
-bot.login(token)
+bot.login(token);
