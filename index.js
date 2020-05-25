3a2c3d80b24f729776bf49a68cca6f54549caecf
Izu <tushar.jamwal@gmail.com>
2020-05-25 21:54:38 +0530

    v0.3.0 Added help command
    Code is now clean and properly stylised.
    Help command is functional.
    Next goal is to make embeds for profiles  with nationality, name, id and profile pictures.

--- index.js
+++ index.js
@@ -6,46 +6,58 @@ const bot = new Discord.Client();
 
 const apiUrl = 'https://www.worldcubeassociation.org/api/v0/';
 const search = 'search/users?persons_table=true&q=';
+let requestUrl = '';
 let qry = '';
 let result;
 
+const help = new Discord.MessageEmbed()
+	.setTitle('**WCA Bot Commands**')
+	.setColor(0xff3e15)
+	.setDescription('All commands must have the "wca?" prefix followed by the command name without any space.')
+	.addField(PREFIX + 'search', 'Searches for WCA profiles based on their name or WCA id\nExample: `' + PREFIX + ' 2015JAMW`')
+	.addField(PREFIX + 'test', 'Asks the WCA bot if it is listening. It will reply to you if it is.');
+
+bot.on('ready', () => {
+	console.log('I am ready!');
+});
+
 bot.on('message', async msg=>{
-    if(msg.content.substring(0,PREFIX.length)===PREFIX){
+	if(msg.content.substring(0, PREFIX.length) === PREFIX){
 
-        let args = msg.content.substring(PREFIX.length).split(" ");
+		const args = msg.content.substring(PREFIX.length).split(' ');
 
-        switch(args[0]){
-            //a case for every supported bot command
+		switch(args[0]){
+		// a case for every supported bot command
 
-            case 'search':
-                qry = args[1];
-                let requestUrl = apiUrl+search+qry;
-                const result = await fetch(requestUrl).then(response => response.json());
-                msg.reply(cuberName(result));
-                break;
+		case 'search':
+			qry = args[1];
+			requestUrl = apiUrl + search + qry;
+			result = await fetch(requestUrl).then(response => response.json());
+			msg.reply(cuberName(result));
+			break;
 
-            case 'help':
-                //insert code for help command here
-                break;
-                
-            case 'test':
-                msg.reply("I am listening");
-                break;
+		case 'help':
+			msg.reply(help);
+			break;
 
-            case:
-                msg.reply('That is not a valid command. Type '+ PREFIX + 'help for a list of commands.')
-        }
-    }    
-})
+		case 'test':
+			msg.reply('I am listening');
+			break;
 
-//declare functions here.
-/*The functionality of search can be moved here
+		default:
+			msg.reply('That is not a valid command. Type `' + PREFIX + 'help` for a list of commands.');
+		}
+	}
+});
+
+// declare functions here.
+/* The functionality of search can be moved here
 making the switch structure for commands cleaner and easier to understand*/
-//I will put the list of functions that accepts json objects as arguments under this
-//have to detach the discord message from this
+// I will put the list of functions that accepts json objects as arguments under this
+// have to detach the discord message from this
 function cuberName(jsonObj){
-    let result = jsonObj['result'];
-    let cuber = result[0];
-    return cuber['name'];
+	result = jsonObj['result'];
+	const cuber = result[0];
+	return cuber['name'];
 }
 bot.login(token);
